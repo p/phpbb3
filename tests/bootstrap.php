2a5fecb0b048d37af04ba54e44cddda006d75005
Igor Wiedler <igor@wiedler.ch>
2012-03-11 15:14:13 +0100

    [feature/event-dispatcher] Use real EventDispatcher through composer
    * replace the copy-pasta EventDispatcher with the real one from Symfony2
    * use composer for managing this dependency, use composer autoloading
    
    PHPBB3-9550

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,6 +13,7 @@ $phpEx = 'php';
 require_once $phpbb_root_path . 'includes/startup.php';
 
 $table_prefix = 'phpbb_';
+require_once $phpbb_root_path . 'vendor/.composer/autoload.php';
 require_once $phpbb_root_path . 'includes/constants.php';
 require_once $phpbb_root_path . 'includes/class_loader.' . $phpEx;
 
