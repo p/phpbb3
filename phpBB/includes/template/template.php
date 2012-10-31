imkingdavid/phpbb

[feature/template-events] Inject extension manager into template class.
Template class passes extension manager to template compiler.

Template compiler passes extension manager to template filter.

Template filter will use extension manager to locate hooks
as it is compiling templates.

All extension manager arguments are optional. If an extension
manager is not given, template hooks will not be invoked.

PHPBB3-9550

phpBB/includes/template/template.php
@@ -81,13 +81,21 @@ class phpbb_template
 	public $template_path = 'template/';
 
 	/**
+	* Extension manager.
+	*
+	* @var phpbb_extension_manager
+	*/
+	private $extension_manager;
+
+	/**
 	* Constructor.
 	*
 	* @param string $phpbb_root_path phpBB root path
 	* @param user $user current user
 	* @param phpbb_template_locator $locator template locator
+	* @param phpbb_extension_manager $extension_manager extension manager, if null then template hooks will not be invoked
 	*/
-	public function __construct($phpbb_root_path, $phpEx, $config, $user, phpbb_template_locator $locator)
+	public function __construct($phpbb_root_path, $phpEx, $config, $user, phpbb_template_locator $locator, phpbb_extension_manager $extension_manager = null)
 	{
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->phpEx = $phpEx;
@@ -95,6 +103,7 @@ class phpbb_template
 		$this->user = $user;
 		$this->locator = $locator;
 		$this->template_path = $this->locator->template_path;
+		$this->extension_manager = $extension_manager;
 	}
 
 	/**
@@ -286,7 +295,7 @@ class phpbb_template
 			return new phpbb_template_renderer_include($output_file, $this);
 		}
 
-		$compile = new phpbb_template_compile($this->config['tpl_allow_php'], $this->locator, $this->phpbb_root_path);
+		$compile = new phpbb_template_compile($this->config['tpl_allow_php'], $this->locator, $this->phpbb_root_path, $this->extension_manager);
 
 		if ($compile->compile_file_to_file($source_file, $output_file) !== false)
 		{

p/phpbb3

phpBB/includes/template/template.php
@@ -81,13 +81,21 @@ class phpbb_template
 	public $template_path = 'template/';
 
 	/**
+	* Extension manager.
+	*
+	* @var phpbb_extension_manager
+	*/
+	private $extension_manager;
+
+	/**
 	* Constructor.
 	*
 	* @param string $phpbb_root_path phpBB root path
 	* @param user $user current user
 	* @param phpbb_template_locator $locator template locator
+	* @param phpbb_extension_manager $extension_manager extension manager, if null then template hooks will not be invoked
 	*/
-	public function __construct($phpbb_root_path, $php_ext, $config, $user, phpbb_template_locator $locator)
+	public function __construct($phpbb_root_path, $php_ext, $config, $user, phpbb_template_locator $locator, phpbb_extension_manager $extension_manager = null)
 	{
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->php_ext = $php_ext;
@@ -95,6 +103,7 @@ class phpbb_template
 		$this->user = $user;
 		$this->locator = $locator;
 		$this->template_path = $this->locator->template_path;
+		$this->extension_manager = $extension_manager;
 	}
 
 	/**
@@ -287,7 +296,7 @@ class phpbb_template
 			return new phpbb_template_renderer_include($output_file, $this);
 		}
 
-		$compile = new phpbb_template_compile($this->config['tpl_allow_php'], $this->locator, $this->phpbb_root_path);
+		$compile = new phpbb_template_compile($this->config['tpl_allow_php'], $this->locator, $this->phpbb_root_path, $this->extension_manager);
 
 		if ($compile->compile_file_to_file($source_file, $output_file) !== false)
 		{